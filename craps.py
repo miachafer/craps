fd631511235e914f7d20ca37dc08383b713f1302
Marilia Ferreira <92002829+miachafer@users.noreply.github.com>
2022-03-30 21:15:16 -0300

    Delete comment explanation about craps rules

--- craps.py
+++ craps.py
@@ -1,16 +1,3 @@
-"""
-Jogo de craps:
-Faça um programa que implemente um jogo de Craps.
-O jogador lança um par de dados, obtendo um valor entre 2 e 12.
-Se você tirar 2, 3 ou 12 na primeira jogada, isto é chamado de "craps" e você perdeu.
-Se, na primeira rodada, você fez um 4, 5, 6, 8, 9 ou 10, este é o seu ponto.
-Seu objetivo agora é continuar jogando os dados até tirar este ponto novamente.
-Você perde no entanto, se tirar um 7 antes de tirar este número novamente.
-"""
-
-### JOGO DE CRAPS 
-
-
 from random import randint
 
 dado1 = randint(1, 6)
